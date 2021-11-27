3f5c4bee91562abea87211bd4ebd1746c7599401
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-11-28 00:31:43 +0800

    Add the Models

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PartsAndAccess(models.Model):
+    parts_name = models.CharField(max_length=255)
+    parts_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.parts_name} {self.parts_price}"
+
+
+class Services(models.Model):
+    serve_name = models.CharField(max_length=255)
+    serve_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.serve_name} {self.serve_price}"
+
+
+class ReservationShed(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    services = models.ManyToManyField(Services)
+    schedule = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.user}"
+
+
+class PartsOrder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    parts = models.ManyToManyField(PartsAndAccess)
+
+    def __str__(self):
+        return f"{self.user}"
