7760a783c93c6796a41eb341c1916794bf69b9f5
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-11-29 01:54:11 +0800

    Add the url route for the Login and Register

--- reservation/urls.py
+++ reservation/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.Home, name="home"),
     path("reservation/", views.Reservation, name="reservation"),
+    # AUTH
+    path("register/", views.Register, name="register"),
+    path("login/", views.Login, name="login"),
+    path("logout/", views.Logout, name="logout"),
 ]
