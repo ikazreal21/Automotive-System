d71fc792653eb576fd4de73ac44423a295a8fe51
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-12-06 00:25:08 +0800

    add the complete url paths

--- reservation/urls.py
+++ reservation/urls.py
@@ -5,6 +5,12 @@ from . import views
 urlpatterns = [
     path("", views.Home, name="home"),
     path("reservation/", views.Reservation, name="reservation"),
+    path("about/", views.About, name="about"),
+    path("contacts/", views.Contact, name="contacts"),
+    path("schedule/", views.Schedule, name="schedule"),
+    path("parts-and-accessories/", views.PartsAccess, name="parts"),
+    path("services/", views.Services, name="services"),
+    path("carwash/", views.Carwash, name="carwash"),
     # AUTH
     path("register/", views.Register, name="register"),
     path("login/", views.Login, name="login"),
