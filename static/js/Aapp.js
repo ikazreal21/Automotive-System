e8f596aa5e6af523b81a8ce1e3065ddd539ef421
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-12-06 00:27:06 +0800

    update Aapp.js

---
+++ static/js/Aapp.js
@@ -0,0 +1,13 @@
+/*DASHBOARD FUNCTIONS*/
+function partsBuy() {
+    return alert("Go to our shop to pay selected item/s");
+  }
+  function ecoWashPrice() {
+    return alert("Eco Wash: ₱ 299");
+  }
+  function premiumWashPrice() {
+    return alert("Premium Wash: ₱ 599");
+  }
+  function goldWashPrice() {
+    return alert("Gold Standard Wash: ₱ 999");
+  }
